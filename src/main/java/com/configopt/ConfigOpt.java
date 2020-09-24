eee7eefea5dd1805f6b7dbb73bbf8232ccce3c91
Samuele Illuminati <samuele8@gmail.com>
2020-09-24 21:37:33 +0200

    parameters

--- src/main/java/com/configopt/ConfigOpt.java
+++ src/main/java/com/configopt/ConfigOpt.java
@@ -1,7 +1,5 @@
 package com.configopt;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,46 +8,55 @@ import com.configopt.Utils.Mode;
 import org.apache.commons.cli.*;
 import org.json.simple.JSONObject;
 
-public class TestPathNode {
+public class ConfigOpt {
     public static void main(String[] args) {
 
+        String usageString = "java -cp target/configopt-1.0-jar-with-dependencies.jar com.configopt.ConfigOpt [options...] --configuration <arg>";
         Options options = new Options();
 
         Option configurationOption = Option.builder("c").longOpt("configuration").required(true)
                 .desc("JSON configuration file path").hasArg().build();
-        Option outputFileOption = Option.builder("o").longOpt("output").required(false).desc("output file").hasArg()
+        Option outputFileOption = Option.builder("o").longOpt("output").required(false).desc("Output file for report").hasArg()
                 .build();
-        Option debugLogLevelOption = Option.builder("v").longOpt("verbose").required(false).desc("verbose logs")
+        Option debugLogLevelOption = Option.builder("v").longOpt("verbose").required(false).desc("Verbose logs")
                 .build();
+        Option interactiveModeOption = Option.builder("i").longOpt("interactive").required(false)
+                .desc("Enables interactive mode").build();
+        Option helpOption = Option.builder("h").longOpt("help").required(false)
+                .desc("Show this help message").build();
 
         // configurationOption.setRequired(true);
-        options.addOption(configurationOption);
-        options.addOption(outputFileOption);
-        options.addOption(debugLogLevelOption);
+        options.addOption(configurationOption).addOption(outputFileOption).addOption(debugLogLevelOption)
+                .addOption(interactiveModeOption).addOption(helpOption);
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd = null;
 
-        System.out.println("args: " + Arrays.toString(args));
         try {
             cmd = parser.parse(options, args);
         } catch (ParseException e) {
             System.out.println(e.getMessage());
-            formatter.printHelp("ConfigOpt", options);
+            formatter.printHelp(usageString, options);
             System.exit(1);
         }
 
+        if(cmd.hasOption(helpOption.getOpt())){
+            formatter.printHelp(usageString, options);
+            System.exit(1);
+        }
         String inputFilePath = cmd.getOptionValue("configuration");
         OutputUtils.outputFile = cmd.getOptionValue("output");
-
         JSONObject JSONConfig = Utils.extractConfigJSONFromFile(inputFilePath);
         List<Service> services = Utils.createServicesFromJSONConfig(JSONConfig);
-
-        if(cmd.hasOption(debugLogLevelOption.getOpt()))
+        if (cmd.hasOption(debugLogLevelOption.getOpt()))
             Logger.getLogger(Utils.LOG_TAG).setLevel(Level.ALL);
         else
             Logger.getLogger(Utils.LOG_TAG).setLevel(Level.SEVERE);
+        if (cmd.hasOption(interactiveModeOption.getOpt()))
+            Utils.mode = Mode.FIXINTERACTIVE;
+        else
+            Utils.mode = Mode.SCAN;
         // Service service1 = new Service(12l, "example.org");
         // service1.addProductMappingRule(new MappingRule("GET", "/foo/bar", 12l));
         // service1.addProductMappingRule(new MappingRule("GET", "/foo/bar/test", 12l));
@@ -65,7 +72,6 @@ public class TestPathNode {
         // services.add(service1);
         // services.add(service2);
 
-        Utils.mode = Mode.SCAN;
         APIcast apicast = new APIcast();
         apicast.setPathRoutingEnabled(true);
         for (Service service : services) {
