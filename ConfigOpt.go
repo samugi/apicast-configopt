a3a07015de115319a185c21457387b3329e073ec
Samuele Illuminati <samuele8@gmail.com>
2020-10-02 17:17:16 +0200

    parameters

--- ConfigOpt.go
+++ ConfigOpt.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+const USAGE string = "usage: go run ConfigOpt.go [options...] --configuration <arg>"
+
 func main() {
-	fmt.Println("Hello world")
+	args := os.Args[1:]
+	if len(args) == 0 {
+		printUsage()
+	}
+
+	fmt.Println("Hello World")
+}
+
+func printUsage() {
+	fmt.Println(USAGE)
 }
